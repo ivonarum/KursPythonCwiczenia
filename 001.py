b77c5e6c1c5e2dae9f032d75d7ed85f35614296e
Iwonarum <ivona.angelika@gmail.com>
2024-12-11 11:15:01 +0100

    1st commit

---
+++ 001.py
@@ -0,0 +1,16 @@
+print("Hello World!") # komentarz
+# komentarz 
+# print("Hello again!")
+
+# 1
+# 2
+# 3
+
+"""
+dowolny 
+tekst 
+w wielu liniach
+!
+"""
+
+print("Hello !")
---
+++ zmienneZadanie.py
@@ -0,0 +1,9 @@
+email = "janek.s@gmail.com"
+title = "masz wiadomosc"
+content = "tekst wiadomosci"
+sender = "Ewa Jankowska"
+liczbaZalacznikow = 2
+nazwaZalacznika = "plik.txt"
+
+print("tytuł maila: " + title)
+print(type(email))
