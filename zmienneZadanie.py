4c6991cb599e25d710ea97f2a5654810739392f1
Iwonarum <ivona.angelika@gmail.com>
2024-12-18 17:27:44 +0100

    2

--- zmienneZadanie.py
+++ zmienneZadanie.py
@@ -7,3 +7,35 @@ nazwaZalacznika = "plik.txt"
 
 print("tytuł maila: " + title)
 print(type(email))
+
+numComlex = 3 + 3j
+print(type(numComlex))
+
+averageNum = (4 + 6 + 3) / 3
+print(averageNum)
+
+bmi = 70 / (1.7 * 1.7)
+print(bmi)
+
+str = "Hello"
+
+#operacje na łańcuchach tekstu:
+print(str[0])
+print(str[1])
+print(str[2:4]) #zakres znakow od do,zakres 'otwarty'
+print(str[0] * 3 + str[4])
+
+tekst = """cos tutaj napisane
+i reszta \"tekstu\"
+w kolejnym wierszu..."""
+print(tekst)
+
+# \n nowa linia
+# \t nowy tabulator
+
+print(len(tekst))
+print(tekst[4:10])
+print(tekst[len(tekst)-1]) # drukowanie ostatniego znaku
+str2 = str + tekst
+print(str2)
+print(str2[7:]) # drukuj od dowolnego znaku do końca
